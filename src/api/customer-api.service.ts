4d34f117792428766d62c48799d6466e79fc8a65
Ali Lozano <alilozanoc@gmail.com>
2023-10-07 09:46:06 -0500

    feat: environment variables added

--- src/api/customer-api.service.ts
+++ src/api/customer-api.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
+import { environment } from '../environments/environment';
 interface Customer {
   id: number,
   name: string,
@@ -18,10 +19,10 @@ export class CustomerApiService {
   httpclient = inject(HttpClient)
 
   async list(): Promise<Customer[]> {
-    return lastValueFrom(this.httpclient.get<Customer[]>('http://localhost:8080/customer'));
+    return lastValueFrom(this.httpclient.get<Customer[]>(`${environment.apiUrl}/customer`));
   }
 
   async save(customer: CustomerSaveRequest): Promise<Customer[]> {
-    return lastValueFrom(this.httpclient.post<Customer[]>('http://localhost:8080/customer', customer));
+    return lastValueFrom(this.httpclient.post<Customer[]>(`${environment.apiUrl}/customer`, customer));
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'http://ec2-54-80-136-240.compute-1.amazonaws.com/'
+};
